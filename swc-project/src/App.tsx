4837269e5475f42aa11936b06a27b980ae76d953
tuugu0730 <121286951+tuugu0730@users.noreply.github.com>
2024-05-11 16:14:00 +0800

    menu

--- swc-project/src/App.tsx
+++ swc-project/src/App.tsx
@@ -1,26 +1,14 @@
 
 import Header from './Header'
-import FoodCard from './FoodCard'
+import Menu from './Menu'
 import './styles/App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <section className="menu">
-        <div className="ttl-menu">
-            <h3 className="ttl">MENU</h3>
-            <button className="btn-all-menu">Бүгдийг харах</button>
-        </div>
-        <div className='foods'>
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-          <FoodCard />
-        </div>
-      </section>
+      <Menu />
+      
     </div>
   )
 }
---
+++ swc-project/src/Menu.tsx
@@ -0,0 +1,20 @@
+import './styles/Menu.css'
+import FoodCard from "./FoodCard"
+
+export default function Menu() {
+    return (
+        <section className="menu">
+            <div className="ttl-menu">
+                <h3 className="ttl">MENU</h3>
+                <button className="btn-all-menu">Бүгдийг харах</button>
+            </div>
+            <div className='foods'>
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+                <FoodCard />
+            </div>
+      </section>
+    )
+}
