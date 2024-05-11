325806d36ac86a4dffeacfbd2abaa3ba3d5080fd
Tuvdennamjil <144209440+Tuvdennamjil@users.noreply.github.com>
2024-05-11 16:41:25 +0800

    update in footer

--- swc-project/src/App.tsx
+++ swc-project/src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header'
+import Footer from './Footer'
 import './styles/App.css'
 import Home from './Home'
+import Restaurant from './Restaurant';
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route  path="/" element={<Home/>}/>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route  path="/" element={<Home/>}/>
+          <Route path="/Restaurant" element={<Restaurant/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
 
-      </Routes>
-    </Router>
-      
+    </>
+    
       
   )
 }
---
+++ swc-project/src/Footer.tsx
@@ -0,0 +1,23 @@
+function Footer(){
+    return(
+        <footer className="footerStyle">
+				<img src="" alt=""/>
+				<div>
+					<h2>Бидэнтэй холбогдох</h2>
+					<p><i/>Gmail</p>
+					<p><i/>Instagram</p>
+					<p><i/>Facebook</p>
+				</div>
+				<div>
+					<h2>Тусламж үйлчилгээ</h2>
+					<p>Асуулт Хариулт</p>
+					<p>Санал хүсэлт</p>
+					<p>Үйлчилгээний нөхцөл</p>
+				</div>
+				<div>
+				</div>
+
+		</footer>
+    )
+}
+export default Footer
